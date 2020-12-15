f37e6cf613a7f112bffa13f0836a9b938298f8dc
Eddie Estevez <eestevez@wisc.edu>
2020-12-15 01:17:22 -0600

    added draggable objects on homepage

---
+++ src/components/DraggableObject.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { motion } from "framer-motion"
+
+function DraggableObject(props) {
+
+    if (props.isImage) {
+        return(
+            <motion.img
+            id = {props.objID}
+            key={props.imageObj}
+            src={props.imageObj}
+            style={{
+                width: props.width,
+                height: props.height,
+                cursor: "grab",
+                borderRadius: 0
+            }}
+            drag
+            dragConstraints={{ top: 0, right: 0, bottom: 0, left: 0 }}
+            dragTransition={{ bounceStiffness: 800, bounceDamping: 25 }}
+            dragElastic={0.75}
+            whileTap={{ cursor: "grabbing" }}
+            />
+        );
+    } else {
+        return(
+            <motion.div
+            id = {props.objID}
+            style={{
+                width: props.width,
+                height: props.height,
+                borderRadius: props.borderRadius,
+                backgroundColor: props.backgroundColor,
+                cursor: "grab"
+            }}
+            drag
+            dragConstraints={{ top: 0, right: 0, bottom: 0, left: 0 }}
+            dragTransition={{ bounceStiffness: 800, bounceDamping: 25 }}
+            dragElastic={0.75}
+            whileTap={{ cursor: "grabbing" }}
+            />
+        );
+    }
+
+}
+
+export default DraggableObject;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,74 @@
 import React from 'react';
-import backgroundPicture from "../images/HomePageBackground.png";
+import blackLineRect from "../images/blackLineRect.svg";
 import Image from "react-bootstrap/Image"
 
+import { Frame } from "framer"
+import { motion } from "framer-motion"
+
 import Hero from "../components/Hero";
+import DraggableObject from "../components/DraggableObject";
+
+const InteractiveDiv = () => (
+    <Frame id="interactiveDivHomePage">
+        <DraggableObject objID = "greenCircleHomePage"
+        isImage={false}
+        width={150}
+        height={150}
+        borderRadius = "100%"
+        backgroundColor="#21eaaf"
+        />
+        <DraggableObject objID = "blueCircleHomePage"
+        isImage={false}
+        width={150}
+        height={150}
+        borderRadius = "100%"
+        backgroundColor="#2196f3"
+        />
+        <DraggableObject objID = "purpleCircleHomePage"
+        isImage={false}
+        width={150}
+        height={150}
+        borderRadius = "100%"
+        backgroundColor="#672fd9"
+        />
+
+        <DraggableObject objID = "blueRectangeHomePage"
+        isImage={false}
+        width={200}
+        height={150}
+        borderRadius = {0}
+        backgroundColor="#2196f3"
+        />
+        <DraggableObject objID = "greenRectangeHomePage"
+        isImage={false}
+        width={200}
+        height={150}
+        borderRadius = {0}
+        backgroundColor="#21eaaf"
+        />
+
+
+        <DraggableObject objID = "blackLineRectHomePage"
+        isImage={true} 
+        imageObj={blackLineRect}
+        width={150}
+        height={150}
+        />
+        <DraggableObject objID = "blackLineRectHomePage2"
+        isImage={true} 
+        imageObj={blackLineRect}
+        width={150}
+        height={150}
+        />
+    </Frame>
+)
 
 function HomePage(props) {
 
     return(
         <div>
         <Hero id="hereOnHomePage" title={props.title} subTitle={props.subTitle} />
-        <Image id="homepageBackground" src={backgroundPicture} rounded className="mx-auto" />
+        <InteractiveDiv/>
         </div>
         )
 } 
