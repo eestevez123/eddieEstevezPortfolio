405a61474c46979b3c11a12271e4e2f2b30f3a81
Eddie Estevez <eestevez@wisc.edu>
2021-07-14 14:38:42 -0500

    finally figured out buttons

--- src/pages/PortfolioPage.js
+++ src/pages/PortfolioPage.js
@@ -1,50 +1,60 @@
-import React, {useCallback} from 'react';
-import {useHistory} from 'react-router-dom';
-import { motion } from "framer-motion"
+import React, {useState} from 'react';
+import ButtonGroup from'react-bootstrap/ButtonGroup';
+import Button from'react-bootstrap/Button';
+import ToggleButton from'react-bootstrap/ToggleButton';
+import ToggleButtonGroup from'react-bootstrap/ToggleButtonGroup';
 
 import Hero from "../components/Hero"
-import Content from "../components/Content";
 import PortfolioItem from "../components/PortfolioItem";
 
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container'
 
 import Fade from 'react-reveal/Fade';
-import Slide from 'react-reveal/Slide';
 
 function PortfolioPage(props) {
 
-    // const history = useHistory();
-    // const handleOnClick = useCallback((title) => history.push("/portfolio/"+title), [history]);
+    const sectionArray = [
+        { name: 'web', value: 0 },
+        { name: 'app', value: 1 },
+        { name: 'video', value: 2 },
+      ];
+
+    const [sectionValue, setSectionValue] = useState(0);
+    const [sectionName, setSectionName] = useState(sectionArray[sectionValue].name);
+
+    function mainButtonClick(e) {
+        setSectionValue(e)
+        setSectionName(sectionArray[e].name)
+    }
+
+    
 
 
     return(<>
             <Hero title={props.title} className="mb-0"/>
-            <Content>
+            <Container>
             <Fade Slide>
                 <h1>So far I've made...</h1>
+                <h2>{sectionName}</h2>
             </Fade>
-                <PortfolioItem  Title="Websites" buttonColor="#21eaaf"/> 
-                <PortfolioItem  Title="Applications" buttonColor="#672fd9"/>
-                <PortfolioItem  Title="Videos" buttonColor="#2196f3"/>
-            
-            {/* <Fade left>
-                <h1 id="subTextPortfolio2">I've also gotten pretty good at using the Adobe Creative Apps!</h1>
-            </Fade>
-            <Slide bottom>
-            <Container id ="adobeAppContainer">
-            <Row id="adobeAppContainerRow">
-            
-                <Col onClick={()=>handleOnClick("Photoshop")} className="adobeAppContainerCol PhotoShop"><motion.div whileHover={{ scale: 1.2 }}>PhotoShop</motion.div></Col>
-                <Col onClick={()=>handleOnClick("Premire")}className="adobeAppContainerCol Premiere"><motion.div whileHover={{ scale: 1.2 }}>Premire Pro</motion.div></Col>
-                <Col onClick={()=>handleOnClick("Aftereffects")} className="adobeAppContainerCol AfterE"><motion.div whileHover={{ scale: 1.2 }}>After Effects</motion.div></Col>
-                <Col onClick={()=>handleOnClick("Audition")}className="adobeAppContainerCol Audition"><motion.div whileHover={{ scale: 1.2 }}>Audtion</motion.div></Col>
-            </Row>
             </Container>
-            </Slide> */}
-
-            </Content>
+            <div className="container">
+                <ToggleButtonGroup name="button" onChange={(e) => mainButtonClick(e)} >
+                    {sectionArray.map((button, idx) => (
+                    <ToggleButton
+                        key={idx}
+                        id={`button-${button.name}`}
+                        type="button"
+                        variant="outline-primary"
+                        name="button"
+                        value={button.value}
+                    >
+                        {button.name}
+                    </ToggleButton>
+                    ))}
+                </ToggleButtonGroup>
+            </div>
+            
         </>
     )
 } 
