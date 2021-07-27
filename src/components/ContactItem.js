0a579b58e131e289dd51953725bfcac94194169b
Eddie Estevez <eestevez@wisc.edu>
2021-07-27 17:54:42 -0500

    animation help

--- src/components/ContactItem.js
+++ src/components/ContactItem.js
@@ -1,18 +1,20 @@
 import React, {useRef, useState} from "react";
 import { useSpring, animated as a } from "@react-spring/web";
+import Skeleton from 'react-loading-skeleton';
 
-const calc = (x, y, rect) => [
+const calc = (x, y, rect, scale) => [
     -(y - rect.top - rect.height / 2) / 5,
     (x - rect.left - rect.width / 2) / 5,
-    1.1
+    scale
   ];
   
 const trans = (x, y, s) =>
   `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
 
 function ContactItem(props) {
-    const ref = useRef(null);
+    const refRect = useRef(null);
     const [xys, set] = useState([0, 0, 1]);
+    const [isLoading, setIsLoading] = useState(true)
   
     const propsSpring = useSpring({ xys });
 
@@ -21,21 +23,25 @@ function ContactItem(props) {
     return(
         <>
         <div className={props.colClass}>
-            <a.div className="text-center animatedDiv bg-myBlue py-5" ref={ref} 
-            style={{ transform: propsSpring.xys.to(trans) }}
-            onMouseLeave={() => set([0, 0, 1])}
-            onMouseMove={(e) => {
-                const rect = ref.current.getBoundingClientRect();
-                set(calc(e.clientX, e.clientY, rect));
-              }}
-            >
-                <a href={props.href} className="text-decoration-none text-white">
+            <a href={props.href} className="text-decoration-none text-white">
+                <a.div className="text-center animatedDiv bg-myBlue rounded py-5" ref={refRect} 
+                style={{ transform: propsSpring.xys.to(trans) }}
+                onMouseLeave={() => set([0, 0, 1])}
+                onMouseMove={(e) => {
+                    const rect = refRect.current.getBoundingClientRect();
+                    set(calc(e.clientX, e.clientY, rect, 1.15));
+                }}
+                onClick={(e) => {
+                    set([0, 0, 0.8]);
+                }}
+                >
                     <div className="icon d-flex align-items-center justify-content-center">
-                        <img src={props.imgAddr} className="fa fa-map-marker contactItemImg" alt={props.imgAlt}/>
+                        {isLoading?(<><Skeleton height={60} className="contactItemImg"/></>):(<></>)}
+                        <img src={props.imgAddr} className={`fa fa-map-marker contactItemImg ${isLoading?("hidden"):("")}`} alt={props.imgAlt} onLoad={()=>{setIsLoading(false)}}/>
                     </div>
                     <p className="text-break"><span>{props.txtTitle}</span>{props.txt}</p>
-                </a>
             </a.div>
+            </a>
         </div>
         </>
     );
