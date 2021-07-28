d82b7f6c3fdfbae7c763f77496bf3edef85b2b99
Eddie Estevez <eestevez@wisc.edu>
2021-07-27 19:29:57 -0500

    updates on the site copy

--- src/portfolioFiles/cards/sounds.js
+++ src/portfolioFiles/cards/sounds.js
@@ -3,14 +3,14 @@ export const soundCards =  [
         src: require("../../images/myWorkPage/cardPics/sounds/PodcastAboutRelationships.png").default,
         alt: "Podcast About Relationships Logo",
         title: "A Podcast About Relationships",
-        desc: "Within CA 155: Introduction to Digital Media Production, I and another student created a mini podcast episode about Long Distance relationships and interviewed some people about their views on them. ",
-        href: "podcast-about-relationships", 
+        desc: "Within CA 155: Introduction to Digital Media Production, I and another student created a mini podcast episode about Long Distance relationships",
+         href: "podcast-about-relationships", 
     },
     {   
         src: require("../../images/myWorkPage/cardPics/sounds/BeepBoopLogo.jpg").default,
         alt: "Beep Boop Logo",
         title: "Beep Boop (Episode 1)",
-        desc: "During my third year (Spring 2020), I took a classed called CA 449: Sound Cultures and Podcasting. The class taught me how to effectively create soundscapes, editing, and sounds in general to effectively convey ideas and emotions. For my final project, I created a short podcast called “Beep Boop” that tackled the question “What is a computer?” in a comedic way. This was an opportunity for me to create a podcast that merged my passion for tech and digital content creation together.",
+        desc: "During my third year (Spring 2020), I took a classed called CA 449: Sound Cultures and Podcasting, and for my final project I created a short podcast called “Beep Boop” that tackled the question “What is a computer?” in a fun and inviting way.",
         href: "beep-boop", 
     },
 ]
