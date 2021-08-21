2211478048cd26cb44b796c2427d3c8f744497c1
Eddie Estevez <eestevez@wisc.edu>
2021-08-20 21:38:58 -0500

    cards do not rerender when windows width changes

--- src/pages/PortfolioPage.jsx
+++ src/pages/PortfolioPage.jsx
@@ -9,12 +9,8 @@ import {
 
 import "./PortfolioPage.css";
 
-import Button from'react-bootstrap/Button';
-import ButtonGroup from'react-bootstrap/ButtonGroup';
-import DropdownButton from 'react-bootstrap/DropdownButton';
-import Dropdown from 'react-bootstrap/Dropdown';
-
 import Hero from "../components/Hero"
+import WorkNav from "../components/WorkNav"
 import Cards from '../components/Cards';
 
 
@@ -22,21 +18,6 @@ import Cards from '../components/Cards';
 import Template from '../portfolioPages/Template';
 
 function PortfolioPage(props) {
-
-    const windowWidth = useWindowWidth();
-    const [isScreenLarge, setIsScreenLarge] = useState(true)
-    // breakpoint at 992px coming down from large screens
-    // large and up --> buttons
-    // medium and below --> dropdown
-    // default --> true, will render out buttons if no parameter is passed
-
-    useEffect(()=> {
-        if (windowWidth >= 992) {
-            setIsScreenLarge(true)
-        } else {
-            setIsScreenLarge(false) 
-        }
-    }, [windowWidth])
 
     const sectionArray = [
         { name: 'Web Development and Design', value: 0 },
@@ -50,12 +31,7 @@ function PortfolioPage(props) {
 
     let { path } = useRouteMatch();
 
-    function mainButtonClick(e) {
-        let val = e.target.value
-        setSectionValue(parseInt(val))
-    }
-
-    function dropDownClick(e) {
+    const mainButtonClick = (e) => {
         let val = e.target.value
         setSectionValue(parseInt(val))
     }
@@ -82,41 +58,8 @@ function PortfolioPage(props) {
             <Switch>
                 <Route exact path={path}>
                     <Hero title={props.title} className="mb-0"/>
-                    <div className="container text-center">
-                    {(isScreenLarge)?(<>
-                        <ButtonGroup className="btn-group-justified" name="button" onClick={(e) => mainButtonClick(e)} >
-                            {sectionArray.map((button, idx) => (
-                            <Button
-                                key={idx}
-                                id={`button-${button.name}`}
-                                type="button"
-                                className={sectionValue === button.value ? 'active' : ''}
-                                variant="outline-primary"
-                                value={button.value}
-                            >
-                                {button.name}
-                            </Button>
-                            ))}
-                        </ButtonGroup>
-                    </>):(<>
-                        <DropdownButton id="dropdown-item-button" title={sectionArray[sectionValue].name}>
-                            {sectionArray.map((button, idx) => (
-                                <Dropdown.Item
-                                    as="button"
-                                    key={idx}
-                                    id={`button-${button.name}`}
-                                    type="button"
-                                    className={sectionValue === button.value ? 'active' : ''}
-                                    variant="outline-primary"
-                                    value={button.value}
-                                    onClick={(e) => dropDownClick(e)}
-                                >
-                                    {button.name}
-                                </Dropdown.Item>
-                                ))}
-                        </DropdownButton>
-                    </>)}
-                    </div>
+                    
+                    <WorkNav section={sectionValue} updateHandler={mainButtonClick} />
                     
                     <Cards section={sectionValue} path={path}/>
                 </Route>
@@ -130,26 +73,5 @@ function PortfolioPage(props) {
         </>
     )
 }
-
-// Hook for checking what the current window size is 
-function useWindowWidth() {
-
-    const [windowWidth, setWindowWidth] = useState(1000)
-    useEffect(() => {
-      // Handler to call on window resize
-      function handleResize() {
-        // Set window width/height to state
-        setWindowWidth(window.innerWidth,
-        );
-      }
-      // Add event listener
-      window.addEventListener("resize", handleResize);
-      // Call handler right away so state gets updated with initial window size
-      handleResize();
-      // Remove event listener on cleanup
-      return () => window.removeEventListener("resize", handleResize);
-    }, []); // Empty array ensures that effect is only run on mount
-    return windowWidth;
-  }
 
 export default PortfolioPage;
